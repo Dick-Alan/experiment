508d56474249b27bdf972cdf4c79dce4146c84a6
richard rekow <richardrekow@gmail.com>
2023-04-30 14:28:51 -0500

    layout.tsx fix

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,6 @@
 import type { PropsWithChildren } from "react";
 
-export const PageLayout = (props: PropsWithChildren<{}>) => {
+export const PageLayout = (props: PropsWithChildren) => {
   return (
     <main className="flex h-screen justify-center">
       <div className="bg-grey-800 h-full w-full  border-lime-600 md:max-w-2xl">
