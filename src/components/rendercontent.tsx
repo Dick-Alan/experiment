75b5363979a39265bb3a3e2f09a4828914404da1
richard rekow <richardrekow@gmail.com>
2023-05-08 15:44:06 -0500

    put linkify back, removed highlight

--- src/components/rendercontent.tsx
+++ src/components/rendercontent.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
-// import Linkify from "react-linkify";
-import Highlight from "react-highlight";
+import Linkify from "react-linkify";
+// import Highlight from "react-highlight";
 export const RenderContent = (props: { content: string }) => {
   const codex = new RegExp(/(?<=``)((.|\n)*)(?=``)/g);
   const codeblock = props.content.match(codex);
@@ -10,31 +10,31 @@ export const RenderContent = (props: { content: string }) => {
 
   return (
     <div className="flex gap-1">
-      {/* <Linkify> */}
-      <pre className="max-w-xl">
-        {text.map((e: string) =>
-          e.includes("$CODEBLOCK") ? (
-            <div key={e} className="border border-green-500 p-1">
-              <div className="border border-white">
-                <Highlight>{codeblock}</Highlight>
+      <Linkify>
+        <pre className="max-w-xl">
+          {text.map((e: string) =>
+            e.includes("$CODEBLOCK") ? (
+              <div key={e} className="border border-green-500 p-1">
+                <div className="border border-white">
+                  <div>{codeblock}</div>
+                </div>
               </div>
-            </div>
-          ) : (
-            <span className="object-contain font-normal" key={e}>
-              {regex.test(e) ? (
-                <img
-                  src={e}
-                  className="hover: m-3 h-auto max-w-[300px]     object-scale-down "
-                  alt={e}
-                ></img>
-              ) : (
-                <>{e} </>
-              )}
-            </span>
-          )
-        )}
-      </pre>
-      {/* </Linkify> */}
+            ) : (
+              <span className="object-contain font-normal" key={e}>
+                {regex.test(e) ? (
+                  <img
+                    src={e}
+                    className="hover: m-3 h-auto max-w-[300px]     object-scale-down "
+                    alt={e}
+                  ></img>
+                ) : (
+                  <>{e} </>
+                )}
+              </span>
+            )
+          )}
+        </pre>
+      </Linkify>
     </div>
   );
 };
