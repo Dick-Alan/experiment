fec0900716e4e0aaed2a88e92a76daf5d16cccad
richard rekow <richardrekow@gmail.com>
2023-05-08 16:16:28 -0500

    reverted the reversion

--- src/components/rendercontent.tsx
+++ src/components/rendercontent.tsx
@@ -1,41 +1,40 @@
 import Link from "next/link";
 import Image from "next/image";
+import Linkify from "react-linkify";
+import Highlight from "react-highlight";
 export const RenderContent = (props: { content: string }) => {
-  const links: string[] =
-    props.content.match(
-      /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/g
-    ) || [];
-
-  const unSplitText = props.content;
-
-  const text: string[] = unSplitText.split(" ");
+  const codex = new RegExp(/(?<=``)((.|\n)*)(?=``)/g);
+  const codeblock = props.content.match(codex);
+  const text: string[] = props.content.replace(codex, "$CODEBLOCK").split(" ");
   const regex = new RegExp(`(?:jpg|png)`);
+
   return (
     <div className="flex gap-1">
-      <pre className="max-w-xl">
-        {text.map((e: string) => (
-          <span className="object-contain font-normal" key={e}>
-            {links.includes(e) ? (
-              regex.test(e) ? (
-                <Link className="h-auto max-w-[300px] object-contain" href={e}>
+      <Linkify>
+        <pre className="max-w-xl">
+          {text.map((e: string) =>
+            e.includes("$CODEBLOCK") ? (
+              <div key={e} className="border border-green-500 p-1">
+                <div className="border border-white">
+                  <Highlight>{codeblock}</Highlight>
+                </div>
+              </div>
+            ) : (
+              <span className="object-contain font-normal" key={e}>
+                {regex.test(e) ? (
                   <img
                     src={e}
                     className="hover: m-3 h-auto max-w-[300px]     object-scale-down "
                     alt={e}
-                  />
-                </Link>
-              ) : (
-                <Link
-                  className="text-purple-500 underline  hover:text-green-500"
-                  href={`${e}`}
-                >{`${e} `}</Link>
-              )
-            ) : (
-              `${e} `
-            )}
-          </span>
-        ))}
-      </pre>
+                  ></img>
+                ) : (
+                  <>{e} </>
+                )}
+              </span>
+            )
+          )}
+        </pre>
+      </Linkify>
     </div>
   );
 };
