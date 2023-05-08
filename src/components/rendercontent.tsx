efeea81a0c588e37dc6ae7c55dacb54ad6b3bbce
richard rekow <richardrekow@gmail.com>
2023-05-08 15:40:41 -0500

    removed linkify

--- src/components/rendercontent.tsx
+++ src/components/rendercontent.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import Linkify from "react-linkify";
+// import Linkify from "react-linkify";
 import Highlight from "react-highlight";
 export const RenderContent = (props: { content: string }) => {
   const codex = new RegExp(/(?<=``)((.|\n)*)(?=``)/g);
@@ -10,31 +10,31 @@ export const RenderContent = (props: { content: string }) => {
 
   return (
     <div className="flex gap-1">
-      <Linkify>
-        <pre className="max-w-xl">
-          {text.map((e: string) =>
-            e.includes("$CODEBLOCK") ? (
-              <div key={e} className="border border-green-500 p-1">
-                <div className="border border-white">
-                  <Highlight>{codeblock}</Highlight>
-                </div>
+      {/* <Linkify> */}
+      <pre className="max-w-xl">
+        {text.map((e: string) =>
+          e.includes("$CODEBLOCK") ? (
+            <div key={e} className="border border-green-500 p-1">
+              <div className="border border-white">
+                <Highlight>{codeblock}</Highlight>
               </div>
-            ) : (
-              <span className="object-contain font-normal" key={e}>
-                {regex.test(e) ? (
-                  <img
-                    src={e}
-                    className="hover: m-3 h-auto max-w-[300px]     object-scale-down "
-                    alt={e}
-                  ></img>
-                ) : (
-                  <>{e} </>
-                )}
-              </span>
-            )
-          )}
-        </pre>
-      </Linkify>
+            </div>
+          ) : (
+            <span className="object-contain font-normal" key={e}>
+              {regex.test(e) ? (
+                <img
+                  src={e}
+                  className="hover: m-3 h-auto max-w-[300px]     object-scale-down "
+                  alt={e}
+                ></img>
+              ) : (
+                <>{e} </>
+              )}
+            </span>
+          )
+        )}
+      </pre>
+      {/* </Linkify> */}
     </div>
   );
 };
