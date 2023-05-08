c162535113a99b8467576b664db184f673061770
richard rekow <richardrekow@gmail.com>
2023-05-08 18:36:56 -0500

    again

--- src/components/rendercontent.tsx
+++ src/components/rendercontent.tsx
@@ -1,32 +1,47 @@
 import Link from "next/link";
-import { useState, useEffect } from "react";
+import { useState, useCallback, useEffect } from "react";
 import Image from "next/image";
 import Linkify from "react-linkify";
 import Highlight from "react-highlight";
+const useMediaQuery = (width) => {
+  const [targetReached, setTargetReached] = useState(false);
 
-export const RenderContent = (props: { content: string }) => {
-  const [width, setWidth] = useState(300);
-  useEffect(() => {
-    if (typeof window === undefined) {
-      return;
+  const updateTarget = useCallback((e) => {
+    if (e.matches) {
+      setTargetReached(true);
+    } else {
+      setTargetReached(false);
     }
-    setWidth(window.innerWidth);
-    const handleResize = (): void => setWidth(window.innerWidth);
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
   }, []);
+
+  useEffect(() => {
+    const media = window.matchMedia(`(max-width: ${width}px)`);
+    media.addListener(updateTarget);
+
+    // Check on mount (callback is not called until a change occurs)
+    if (media.matches) {
+      setTargetReached(true);
+    }
+
+    return () => media.removeListener(updateTarget);
+  }, []);
+
+  return targetReached;
+};
+export const RenderContent = (props: { content: string }) => {
+  const isBreakpoint = useMediaQuery(768);
   const codex = new RegExp(/(?<=``)((.|\n)*)(?=``)/g);
   const codeblock = props.content.match(codex);
   const text: string[] = props.content.replace(codex, "$CODEBLOCK").split(" ");
   const regex = new RegExp(`(?:jpg|png)`);
-  const screenwidth = width >= 775 ? "xl" : "sm";
+  // const screenwidth = width >= 775 ? "xl" : "sm";
 
-  console.log(screenwidth, typeof screenwidth, window.innerWidth, width);
+  // console.log(screenwidth, typeof screenwidth, window.innerWidth, width);
 
   return (
     <div className={` flex gap-1`}>
       <Linkify>
-        <pre className={`max-w-sm`}>
+        <pre className={`max-w-${isBreakpoint ? "sm" : "xl"}`}>
           {text.map((e: string) =>
             e.includes("$CODEBLOCK") ? (
               <div key={e} className="border border-green-500 p-1">
